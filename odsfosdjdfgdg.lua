6f367a6301c859ff24a15fb1c60833800a883ee0
asdhksdfjkhg3g2jk2g <wplay849@gmail.com>
2025-11-08 11:58:38 +0700

    Update odsfosdjdfgdg.lua

--- odsfosdjdfgdg.lua
+++ odsfosdjdfgdg.lua
@@ -11,7 +11,7 @@ local runservice = game:GetService("RunService")
 local tweenservice = game:GetService("TweenService")
 local marketplaceservice = game:GetService("MarketplaceService")
 local textservice = game:GetService("TextService")
-local coregui = game:GetService("CoreGui")
+local coregui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 local httpservice = game:GetService("HttpService")
 
 local player = players.LocalPlayer
